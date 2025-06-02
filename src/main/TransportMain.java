8b7f51b757d2de8943076b9a9a783809deeb8603
Mladen <49067795+MladenGrbic@users.noreply.github.com>
2025-06-02 16:15:16 +0200

    Update TransportMain.java
    Uklanjanje bespotrebnog komentara

--- src/main/TransportMain.java
+++ src/main/TransportMain.java
@@ -11,7 +11,6 @@ import main.transport.Network;
 
 import java.io.IOException;
 
-/* -global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository.*/
 
 /**
  * Glavna klasa JavaFX aplikacije za planiranje transportnih ruta.
